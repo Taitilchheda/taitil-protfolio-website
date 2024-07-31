4ca043ac4d9cd0623a8f64e67894d8a40e7cde44
Taitil Chheda <taitilchheda@gmail.com>
2024-07-31 23:43:55 +0530

    back to old

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,18 +7,19 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import python from "./tech/python.png";
-import tensorflow from "./tech/tensorflow.png";
-import pytorch from "./tech/pytorch.png";
-import keras from "./tech/keras.png";
-import numpy from "./tech/numpy.png";
-import pandas from "./tech/pandas.png";
-
-import html from "./tech/html.png";
 import css from "./tech/css.png";
-import javascript from "./tech/javascript.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -39,9 +40,18 @@ export {
   menu,
   close,
   css,
+  docker,
+  figma,
   git,
   html,
   javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
   meta,
   shopify,
   starbucks,
@@ -49,10 +59,4 @@ export {
   carrent,
   jobit,
   tripguide,
-  python,
-  tensorflow,
-  pytorch,
-  keras,
-  numpy,
-  pandas,
 };
